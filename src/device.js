8c1a93c459edc217ecca69989e953c28fa241141
Ozi <ozisadiqkasai@gmail.com>
2024-11-08 23:25:58 +0100

    modified code

--- src/device.js
+++ src/device.js
@@ -5,7 +5,8 @@ const sizes = {
 }
 
 export const device = {
-    mobile: `(max-width: ${sizes.mobile})`,
+    // mobile: `(max-width: ${sizes.mobile})`,
+    mobile: `(min-width: 320px) and (max-width: ${sizes.mobile})`,
     tablet: `(min-width: 768px) and (max-width: ${sizes.tablet})`,
     desktop: `(min-width: ${sizes.desktop})`
   };
